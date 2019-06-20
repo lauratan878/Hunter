75b068ad2bc10a34f497033b2e66ab5d56c67333
Apple <apple@Apples-MacBook-Pro-41.local>
2019-06-20 13:40:09 -0700

    iteration0

---
+++ ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  Hunter
+//
+//  Created by Apple on 6/20/19.
+//  Copyright © 2019 Laura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var appTitle: UILabel!
+    @IBOutlet weak var omg: UITextField!
+    @IBOutlet weak var donnotTellMe: UITextField!
+    
+    @IBOutlet weak var mindBlown: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func OMG(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func ok(_ sender: UIButton) {
+    }
+    
+    @IBAction func mind(_ sender: UIButton) {
+    }
+    
+    @IBAction func doNotWantToKnow(_ sender: UIButton) {
+    }
+    
+    }
+    
+
+
