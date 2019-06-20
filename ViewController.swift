653bc7caa98058ea414d0d6da7a08b389deac12b
Apple <apple@Apples-MacBook-Pro-41.local>
2019-06-20 14:08:02 -0700

    iteration1

--- ViewController.swift
+++ ViewController.swift
@@ -35,7 +35,20 @@ class ViewController: UIViewController {
     @IBAction func doNotWantToKnow(_ sender: UIButton) {
     }
     
-    }
+    
     
 
+@IBAction func showMessage(sender: UIButton) {
+    let alertController = UIAlertController()
+    alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+    // when click emoji button user should find a text message appear with the proper words the monkey with hands on the mouth should appear  OMG. the monkey just smiling will respond with an ok. the monkey with hands on its ears will respond with mindblown! the monkey with its hands over its eyes will respond with do not show me my exam grades.
+    
+    
 
+    present(alertController, animated: true, completion: nil)
+    //ince we didn’t establish traditional actions between the StoryBoard and code, we need to manually connect them.
+    
+    //While you hold the control button down, click and drag from a button up to the little yellow circle at the top of your StoryBoard scene. When you release, you’ll see a black box with options. Select showMessageWithSender:. Repeat that for every emoji button on your UI. This will connect each button with the showMessage action/function.
+}
+
+}
